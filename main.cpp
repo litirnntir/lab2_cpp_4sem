8dac7ae05c083eda70ab66f1b7583b392b51218d
salnyed <agorbuncova2004@gmail.com>
2023-05-04 15:12:58 +0400

    add selection sort

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <vector>
+
+struct stats
+{
+	size_t comparisonCount = 0, copyCount = 0, time = 0;
+	void operator+=(const stats& rhs)
+	{
+		comparisonCount += rhs.comparisonCount;
+		copyCount += rhs.copyCount;
+		time += rhs.time;
+	}
+};
+
+stats selSort(std::vector<int>:: iterator firstIter, std::vector<int>::iterator secondIter)
+{
+	stats flag;
+	flag.copyCount++;
+	auto tmp = firstIter;
+	while(tmp <= secondIter)
+	{
+		flag.comparisonCount += 2;
+		flag.copyCount++;
+		auto small_ = tmp;
+		if (tmp != secondIter)
+		{
+			flag.copyCount++;
+			auto iter_ = tmp + 1;
+			while (iter_ <= secondIter)
+			{
+				flag.comparisonCount += 2;
+				if (*iter_ < *small_)
+				{
+					flag.copyCount++;
+					small_ = iter_;
+				}
+				iter_++;
+			}
+			flag.comparisonCount++;
+		}
+		std::swap(*(tmp), *(small_));
+		flag.copyCount += 3;
+		tmp++;
+	}
+	flag.comparisonCount++;
+	return flag;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::vector<int> a;
+	a.push_back(1);
+	a.push_back(0);
+	a.push_back(40);
+	a.push_back(111);
+	selSort(a.begin(), a.end());
 	return 0;
 }
