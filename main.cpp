6d7693ca84fe04ee7ee95bcec92061e0df250479
salnyed <agorbuncova2004@gmail.com>
2023-05-17 21:39:00 +0400

    Add merge sort

--- main.cpp
+++ main.cpp
@@ -104,12 +104,71 @@ stats selectionSort(std::vector<int>::iterator begin, std::vector<int>::iterator
 	return stat;
 }
 
+stats merge(std::vector<int>::iterator begin, std::vector<int>::iterator middle, std::vector<int>::iterator end)
+{
+
+	std::vector<int> left_vec(begin, middle), right_vec(middle, end);
+	auto left = left_vec.begin(), right = right_vec.begin(), temp = begin;
+
+	stats tmp;
+
+	while (left < left_vec.end() && right < right_vec.end())
+	{
+		tmp.comparisonCount++;
+		if (*left <= *right)
+		{
+			*temp = *left;
+			left++;
+		}
+		else
+		{
+			*temp = *right;
+			right++;
+		}
+		temp++;
+		tmp.copyCount++;
+	}
+	while (left < left_vec.end())
+	{
+		*temp = *left;
+		left++;
+		temp++;
+		tmp.copyCount++;
+	}
+	while (right < right_vec.end())
+	{
+		*temp = *right;
+		right++;
+		temp++;
+		tmp.copyCount++;
+	}
+	return tmp;
+}
+
+stats naturalMergeSort(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	stats stat;
+	if (end - begin <= 1)
+	{
+		return stat;
+	}
+	auto middle = begin + (end - begin) / 2;
+	stats left_stats = naturalMergeSort(begin, middle);
+	stats right_stats = naturalMergeSort(middle, end);
+	stats result = merge(begin, middle, end);
+	result.comparisonCount += left_stats.comparisonCount + right_stats.comparisonCount;
+	result.copyCount += left_stats.copyCount + right_stats.copyCount;
+
+	return result;
+}
+
 stats testQuickSortRandom(int count)
 {
 	stats stat;
 	for (int j = 0; j < 100; j++)
 	{
 		std::vector<int> vec;
+		vec.reserve(count);
 		for (int i = 0; i < count; i++)
 		{
 			vec.push_back(lcg());
@@ -135,7 +194,8 @@ stats testQuickSortSorted(int count)
 {
 	stats stat;
 	std::vector<int> vec;
-	for (int i = 0; i < count; i++)
+	vec.reserve(count);
+for (int i = 0; i < count; i++)
 	{
 		vec.push_back(i);
 	}
@@ -187,6 +247,7 @@ stats testSelectionSortRandom(int count)
 	for (int j = 0; j < 100; j++)
 	{
 		std::vector<int> vec;
+		vec.reserve(count);
 		for (int i = 0; i < count; i++)
 		{
 			vec.push_back(lcg());
@@ -212,6 +273,7 @@ stats testSelectionSortSorted(int count)
 {
 	stats stat;
 	std::vector<int> vec;
+	vec.reserve(count);
 	for (int i = 0; i < count; i++)
 	{
 		vec.push_back(i);
@@ -251,6 +313,85 @@ stats testSelectionSortReverseSorted(int count)
 	stat.time += duration.count();
 
 	std::cout << "Selection Sort for reverse sorted array with " << count << " numbers" <<
+			  "\n\tTime: " << (double)stat.time / 100
+			  << " microseconds" <<
+			  "\n\tComparisonCount : " << stat.comparisonCount / 100 <<
+			  "\n\tCopyCount : " << stat.copyCount / 100 << std::endl;
+	return stat;
+}
+
+stats testMergeSortRandom(int count)
+{
+	stats stat;
+	for (int j = 0; j < 100; j++)
+	{
+		std::vector<int> vec;
+		vec.reserve(count);
+		for (int i = 0; i < count; i++)
+		{
+			vec.push_back(lcg());
+		}
+
+		auto start = std::chrono::high_resolution_clock::now();
+		stat += naturalMergeSort(vec.begin(), vec.end());
+		auto stop = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+		stat.time += duration.count();
+	}
+	std::cout << "Natural Merge Sort for random array with " << count << " numbers" <<
+			  "\n\tTime: " << (double)stat.time / 100
+			  << " microseconds"
+			  <<
+			  "\n\tComparisonCount : " << stat.comparisonCount / 100 <<
+			  "\n\tCopyCount : " << stat.copyCount / 100 << std::endl;
+	return stat;
+}
+
+stats testMergeSortSorted(int count)
+{
+	stats stat;
+	std::vector<int> vec;
+	vec.reserve(count);
+for (int i = 0; i < count; i++)
+	{
+		vec.push_back(i);
+	}
+
+	auto start = std::chrono::high_resolution_clock::now();
+	stat += naturalMergeSort(vec.begin(), vec.end());
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+	stat.time += duration.count();
+
+	std::cout << "Natural Merge Sort for sorted array with " << count << " numbers" <<
+			  "\n\tTime: " << (double)stat.time / 100
+			  << " microseconds"
+			  <<
+			  "\n\tComparisonCount : " << stat.comparisonCount / 100 <<
+			  "\n\tCopyCount : " << stat.copyCount / 100 << std::endl;
+	return stat;
+
+}
+
+stats testMergeSortReverseSorted(int count)
+{
+	stats stat;
+	std::vector<int> vec;
+	for (int i = count; i > 0; i--)
+	{
+		vec.push_back(i);
+	}
+
+	auto start = std::chrono::high_resolution_clock::now();
+	stat += naturalMergeSort(vec.begin(), vec.end());
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+	stat.time += duration.count();
+
+	std::cout << "Natural Merge Sort for reverse sorted array with " << count << " numbers" <<
 			  "\n\tTime: " << (double)stat.time / 100
 			  << " microseconds" <<
 			  "\n\tComparisonCount : " << stat.comparisonCount / 100 <<
@@ -315,6 +456,22 @@ int main()
 		}
 		if (choice == 3)
 		{
+			int n, i = 0, k;
+			std::vector<int> test;
+			std::cout << "Enter the amount of numbers: ";
+			n = checkIsInt();
+			std::cout << std::endl;
+			std::cout << "Input numbers: ";
+			while (i < n)
+			{
+				k = checkIsInt();
+				test.push_back(k);
+				i++;
+			}
+			stats tmp = naturalMergeSort(test.begin(), test.end());
+			std::cout << "comparisonCount: " << tmp.comparisonCount << " " << "copyCount: " << tmp.copyCount
+					  << std::endl;
+			printVector(test.begin(), test.end());
 		}
 		if (choice == 4)
 		{
@@ -348,6 +505,22 @@ int main()
 				tmp1 = testSelectionSortRandom(i);
 				tmp2 = testSelectionSortSorted(i);
 				tmp3 = testSelectionSortReverseSorted(i);
+				file1 << i << " " << tmp1.comparisonCount << " " << tmp2.comparisonCount << " "
+					  << tmp3.comparisonCount << "\n";
+				file2 << i << " " << tmp1.copyCount << " " << tmp2.copyCount << " " << tmp3.copyCount << "\n";
+				file3 << i << " " << tmp1.time << " " << tmp2.time << " " << tmp3.time << "\n";
+			}
+			file1.close();
+			file2.close();
+			file3.close();
+			file1.open("/Users/aleksandragorbuncova/CLionProjects/salnyed/lab2-cpp-4sem/comparisonCountMergeSort.txt");
+			file2.open("/Users/aleksandragorbuncova/CLionProjects/salnyed/lab2-cpp-4sem/comparisonCountMergeSort.txt");
+			file3.open("/Users/aleksandragorbuncova/CLionProjects/salnyed/lab2-cpp-4sem/timeMergeSort.txt");
+			for (auto i : numbs)
+			{
+				tmp1 = testMergeSortRandom(i);
+				tmp2 = testMergeSortSorted(i);
+				tmp3 = testMergeSortReverseSorted(i);
 				file1 << i << " " << tmp1.comparisonCount << " " << tmp2.comparisonCount << " "
 					  << tmp3.comparisonCount << "\n";
 				file2 << i << " " << tmp1.copyCount << " " << tmp2.copyCount << " " << tmp3.copyCount << "\n";
